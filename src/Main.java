afb5aae12e31de97b2191360e4ea5cfd921df66c
gabe-ki70 <gabriel_koerner-imas26@milton.edu>
2024-09-30 20:35:41 -0400

    Added mainframe and began making button functions

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Scanner;
-public class Main {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+import static java.awt.BorderLayout.CENTER;
 
-        public static void main(String[] args) {
-            Main html = new Main();
-        }
+public class Main implements ActionListener {
 
-        public Main() {
+    private JFrame mainFrame;
+    private JPanel controlPanel;
+    private JLabel statusLabel;
+    private JMenuBar mb;
+    private JMenu file, edit, help;
+    private JMenuItem cut, copy, paste, selectAll;
+    private JTextArea ta; //typing area
+    private int WIDTH = 800;
+    private int HEIGHT = 700;
 
+    public static void main(String[] args) {
+        Main html = new Main();
+        Main.showEventDemo();
+    }
+
+    public Main() {
+        prepareGUI();
+    }
+
+        private void prepareGUI () {
+            mainFrame = new JFrame("Gabe Learning SWING");
+            mainFrame.setSize(WIDTH, HEIGHT);
+            mainFrame.setLayout(new BorderLayout());
+            Button startbutton = new Button("Start");
+            mainFrame.add(startbutton);
             try {
                 System.out.println();
                 System.out.print("hello \n");
@@ -25,32 +52,65 @@ public class Main {
                         new InputStreamReader(url.openStream())
                 );
                 String line;
-                while ( (line = reader.readLine()) != null ) {
+                while ((line = reader.readLine()) != null) {
                     if (line.contains("href")) {
-                        int start = line.indexOf("href=")+6;
+                        int start = line.indexOf("href=") + 6;
                         String link = line.substring(start);
 
                         int end = link.indexOf("\"");
-                        if (end==-1){
+                        if (end == -1) {
                             end = link.indexOf("'");
                         }
-                        if (end==-1){
+                        if (end == -1) {
                             end = link.indexOf("--");
                         }
-                        link = line.substring(start, start+end);
-                        if (link.contains(userKeyword)){
-                        System.out.println(start + "," + end);
-                        //System.out.println(link);
-                        System.out.println(line.substring(start, start+end));
+                        link = line.substring(start, start + end);
+                        if (link.contains(userKeyword)) {
+                            System.out.println(start + "," + end);
+                            //System.out.println(link);
+                            System.out.println(line.substring(start, start + end));
                         }
                     }
                 }
                 reader.close();
-            } catch(Exception ex) {
+
+            } catch (Exception ex) {
                 System.out.println(ex);
             }
+            mainFrame.addWindowListener(new WindowAdapter() {
+                public void windowClosing(WindowEvent windowEvent) {
+                    System.exit(0);
+                }
+            });
 
         }
+    private static void showEventDemo() {
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == cut)
+            ta.cut();
+        if (e.getSource() == paste)
+            ta.paste();
+        if (e.getSource() == copy)
+            ta.copy();
+        if (e.getSource() == selectAll)
+            ta.selectAll();
+    }
+
+    private class ButtonClickListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String command = e.getActionCommand();
+
+            if (command.equals("OK")) {
+                statusLabel.setText("Ok Button clicked.");
+            } else if (command.equals("Submit")) {
+                statusLabel.setText("Submit Button clicked.");
+            } else {
+                statusLabel.setText("Cancel Button clicked.");
+            }
+        }
+    }
     }
 
