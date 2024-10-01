9b4262f7bd7c91153a68865af6d2e48ec0248ad9
gabe-ki70 <gabriel_koerner-imas26@milton.edu>
2024-10-01 11:25:23 -0400

    Started Creating Layout

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import com.sun.jdi.request.MonitorContendedEnteredRequest;
+
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -9,17 +12,18 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import static java.awt.BorderLayout.CENTER;
-
 public class Main implements ActionListener {
 
     private JFrame mainFrame;
     private JPanel controlPanel;
+    private JPanel controlPanel2;
     private JLabel statusLabel;
     private JMenuBar mb;
     private JMenu file, edit, help;
     private JMenuItem cut, copy, paste, selectAll;
     private JTextArea ta; //typing area
+    private JTextArea linkinput;
+    private JTextArea keywordinput;
     private int WIDTH = 800;
     private int HEIGHT = 700;
 
@@ -35,48 +39,21 @@ public class Main implements ActionListener {
         private void prepareGUI () {
             mainFrame = new JFrame("Gabe Learning SWING");
             mainFrame.setSize(WIDTH, HEIGHT);
-            mainFrame.setLayout(new BorderLayout());
-            Button startbutton = new Button("Start");
-            mainFrame.add(startbutton);
-            try {
-                System.out.println();
-                System.out.print("hello \n");
-                Scanner linkholder = new Scanner(System.in);
-                System.out.println("Enter Link: ");
-                String userLink = linkholder.nextLine();
-                Scanner keyword = new Scanner(System.in);
-                System.out.println("Search for Keyword: ");
-                String userKeyword = linkholder.nextLine();
-                URL url = new URL(userLink);
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(url.openStream())
-                );
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    if (line.contains("href")) {
-                        int start = line.indexOf("href=") + 6;
-                        String link = line.substring(start);
+            mainFrame.setLayout(new GridLayout(1, 2));
+            controlPanel = new JPanel();
+            controlPanel2 = new JPanel();
+            controlPanel.setLayout(new GridLayout(1, 1));
+            controlPanel2.setLayout(new GridLayout(2, 1));
 
-                        int end = link.indexOf("\"");
-                        if (end == -1) {
-                            end = link.indexOf("'");
-                        }
-                        if (end == -1) {
-                            end = link.indexOf("--");
-                        }
-                        link = line.substring(start, start + end);
-                        if (link.contains(userKeyword)) {
-                            System.out.println(start + "," + end);
-                            //System.out.println(link);
-                            System.out.println(line.substring(start, start + end));
-                        }
-                    }
-                }
-                reader.close();
 
-            } catch (Exception ex) {
-                System.out.println(ex);
-            }
+            linkinput = new JTextArea("Link: ");
+            keywordinput = new JTextArea("Keyword: ");
+            mainFrame.add(controlPanel2);
+            mainFrame.add(controlPanel);
+            controlPanel2.add(linkinput);
+            controlPanel2.add(keywordinput);
+
+            mainFrame.setVisible(true);
             mainFrame.addWindowListener(new WindowAdapter() {
                 public void windowClosing(WindowEvent windowEvent) {
                     System.exit(0);
@@ -97,6 +74,8 @@ public class Main implements ActionListener {
             ta.copy();
         if (e.getSource() == selectAll)
             ta.selectAll();
+
+
     }
 
     private class ButtonClickListener implements ActionListener {
@@ -107,10 +86,51 @@ public class Main implements ActionListener {
                 statusLabel.setText("Ok Button clicked.");
             } else if (command.equals("Submit")) {
                 statusLabel.setText("Submit Button clicked.");
-            } else {
+            }
+            else {
                 statusLabel.setText("Cancel Button clicked.");
             }
         }
     }
+
+   // try {
+   //     System.out.println();
+   //     System.out.print("hello \n");
+   //     Scanner linkholder = new Scanner(System.in);
+   //     System.out.println("Enter Link: ");
+   //     String userLink = linkholder.nextLine();
+   //     Scanner keyword = new Scanner(System.in);
+   //     System.out.println("Search for Keyword: ");
+   //     String userKeyword = linkholder.nextLine();
+   //     URL url = new URL(userLink);
+   //     BufferedReader reader = new BufferedReader(
+   //             new InputStreamReader(url.openStream())
+   //     );
+    //    String line;
+    //    while ((line = reader.readLine()) != null) {
+    //        if (line.contains("href")) {
+    //            int start = line.indexOf("href=") + 6;
+    //            String link = line.substring(start);
+//
+  //              int end = link.indexOf("\"");
+    //            if (end == -1) {
+     //               end = link.indexOf("'");
+     //           }
+     //           if (end == -1) {
+     //               end = link.indexOf("--");
+     //           }
+     //           link = line.substring(start, start + end);
+     //           if (link.contains(userKeyword)) {
+     //               System.out.println(start + "," + end);
+     //               //System.out.println(link);
+     //               System.out.println(line.substring(start, start + end));
+     //           }
+      //      }
+       // }
+      //  reader.close();
+
+   // } catch (Exception ex) {
+     //   System.out.println(ex);
+    //}
     }
 
